56438d3f86206e39ca23451c8a828be6f88c481a
Oliver Lavers <oliver.f.lavers@gmail.com>
2025-10-27 11:38:11 +1000

    fixed boilerplate code to include placeholders for required input

--- src/she_codes_weather/widgets/daily_summary/daily_summary.py
+++ src/she_codes_weather/widgets/daily_summary/daily_summary.py
@@ -1,4 +1,8 @@
 from she_codes_weather.utils.html_widget import HTMLWidget
 
 class DailySummaryWidget(HTMLWidget):
-    pass
+    
+    template_path=None
+
+    def __init__(self, source_data):
+        pass
