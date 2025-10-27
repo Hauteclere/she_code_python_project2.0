ebd1f2db745a8bd610a226a4d0cbb7c039e5f3c1
Oliver Lavers <oliver.f.lavers@gmail.com>
2025-10-27 11:35:57 +1000

    included date and time in example content

--- src/she_codes_weather/widgets/date_and_time/date_and_time.py
+++ src/she_codes_weather/widgets/date_and_time/date_and_time.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from she_codes_weather.utils.html_widget import HTMLWidget
 
 class DateAndTimeWidget(HTMLWidget):
-    pass
+    template_path="./date_and_time.html"
+
+    def __init__(self):
+        self.date_and_time = datetime.now(tz=ZoneInfo("Australia/Brisbane"))
